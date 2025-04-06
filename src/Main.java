9814b59e93bda52640e63db232879972cc0c583a
AllenDiaz <allengdiaz2019@gmail.com>
2025-04-06 20:33:51 +0800

    recall java constructor

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 public class Main {
-
     public static void main(String[] args) {
 
-        Car car = new Car();
-        
-        System.out.println(car.isRunning);
-        car.start();
-        System.out.println(car.isRunning);
-        car.stop();
-        System.out.println(car.isRunning);
+        // constuctor  = A special method to initialize object
+        //               you can pass agrument to constructor
+        //               and  set up initial values
+
+        Student student = new Student("Allen", 25, 2);
+        System.out.println(student.name);
+        System.out.println(student.age);
+        System.out.println(student.gpa);
+        System.out.println(student.isEnrolled);
     }
 }
