3e01d7cb56dcfadbcdb5c3ca37c94500fc715a8b
AllenDiaz <allengdiaz2019@gmail.com>
2025-04-09 12:21:48 +0800

    Java refresh learning static

---
+++ src/Friend.java
@@ -0,0 +1,11 @@
+public class Friend {
+    
+    static int numOfFriends;
+    String name;
+
+    Friend(String name){
+        this.name = name;
+        numOfFriends++;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-     Car car1 = new Car("Mustang", "Red");
-     Car car2 = new Car("Honda", "Green");
-     Car car3 = new Car("Corvette", "Blue");
+    /* 
+     * static makes a variable or method belong to class 
+     * rather than a specific object commonly used for utility methods
+     * or shared resources
+     */
+        Friend friend1 = new Friend("Allen");   
+        Friend friend2 = new Friend("Patrick");   
+        
+        System.out.println(Friend.numOfFriends);
 
-     Car[] cars = {car1, car2, car3};
-
-    for (Car car : cars){
-        car.color = "black";
-    }
-
-    for (Car car : cars){
-        car.drive();
-    }
     }
 }
